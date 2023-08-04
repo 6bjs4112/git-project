776e21c7d87b322d244ba8dc4c8fc4bf46b23d1e
6bjs4112 <tsunenikannsya@gmail.com>
2023-08-04 18:21:20 +0900

    api domain

--- LionHill/testAPI.js
+++ LionHill/testAPI.js
@@ -34,3 +34,43 @@ function onSignIn(googleUser) {
 function onSignInFailure(t){		
 	console.log(t);
 }
+
+//카카오 로그인===========================================
+Kakao.init('1f700e0243a25038a78fbe54526b04f5'); //발급받은 키 중 javascript키를 사용해준다.
+console.log(Kakao.isInitialized()); // sdk초기화여부판단
+//카카오로그인
+function kakaoLogin() {
+    Kakao.Auth.login({
+      success: function (response) {
+        Kakao.API.request({
+          url: '/v2/user/me',
+          success: function (response) {
+        	  console.log(response)
+          },
+          fail: function (error) {
+            console.log(error)
+          },
+        })
+      },
+      fail: function (error) {
+        console.log(error)
+      },
+    })
+  }
+//카카오로그아웃  
+function kakaoLogout() {
+    if (Kakao.Auth.getAccessToken()) {
+      Kakao.API.request({
+        url: '/v1/user/unlink',
+        success: function (response) {
+        	console.log(response)
+        },
+        fail: function (error) {
+          console.log(error)
+        },
+      })
+      Kakao.Auth.setAccessToken(undefined)
+    }
+  }  
+//카카오맵================
+
