82ea4bac2235b532da26f3b75d52779a5eb1aee9
6bjs4112 <tsunenikannsya@gmail.com>
2023-10-17 18:20:33 +0900

    10/17 list 완료
    write푸터에 가려지던것 수정
    rank수정
    router title에 닉네임만 들어오게 수정
    리스트 최신순으로 나오게
    더보기 버튼(10개씩)
    리스트 반응형 완료

--- DM/app/pages/borad/view/page.js
+++ DM/app/pages/borad/view/page.js
@@ -1,9 +1,87 @@
-import React from 'react'
+"use client"
+import { useRouter } from 'next/navigation';
+import style from './page.module.scss'
+import React, { useEffect, useRef, useState } from 'react'
+import axios from 'axios';
+import {user_get} from '../../../comp/member/Login'
+// import Link from 'next/link';
+import Footer from '@/app/comp/Footer';
+// import LoginCheck from '@/app/comp/LoginCheck';
 
 export default function page() {
+  // url에서 받아오기~~~~
+  // const router = useRouter();
+  // const { id } = router.query;
+
+  //회원정보
+  const [member,setMember] = useState();
+  const [rk,setRk] = useState();
+
+  async function fetchData() {
+      const mb = await user_get()
+      setRk(mb.rk.data)
+      setMember(mb.data);
+  }
+
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  //get으로 DB정보 가져오기
+  const [data,setData] = useState([]);
+
+  const getFile = async function(){
+    try {
+      const dbData = await axios.get('/api/borad/list');
+      setData(dbData.data);
+    } catch (error) {
+      console.error("AxiosError:", error);
+    }
+  }
+  useEffect(()=>{
+    getFile();
+  },[])
+
+  if(!member) return <></>
   return (
-    <div>
+    <article className={style.board_write}>
+      {/* <LoginCheck />  */}
+      <header>
+        <figure className={style.logo}><img src='/img/board/write/logo.png'/></figure>
+        <div className={style.profile} >
+          <img className={style.pfDecoBox} src='/img/board/write/profilebox.png'/>
+          <div className={style.pfInner}>
+            <p>[Rk.{rk}]</p>
+            <figure className={style.pfNickname}>
+              <img src={`/img/main/icon/${member.mb_icon}.png`}/>
+              <figcaption>{member.mb_nick}</figcaption>
+            </figure>
+            <div className={style.pfPictureWrap}>
+              <div className={style.pfPicture}>
+                <img className={style.pfPic} src={`/img/main/face/${member.mb_img}.png`}/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <section className={style.title}>
+        <div className={style.titleTab}>
+          <img className={style.titleDeco} src='/img/board/write/mainDMtab.png'/>
+          <h1>DIGIMON MIND (D.M)</h1>
+        </div>
+      </section>
+
+      <section className={style.drawing}>
+        <div className={style.canvasWrapper}>
+          <img className={style.canvasDeco} src='/img/board/write/canvas_deco.png'/>
+          {/* <img className={style.canvasDeco} src='/img/board/write/canvas_deco.png'/> */}
+        </div>
+      </section>
+
       
-    </div>
+
+      <Footer/>
+    </article>
   )
 }
